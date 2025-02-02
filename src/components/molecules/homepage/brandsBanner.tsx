98c427548bae32647a6d5c5abe4f14916ee149c9
Giorgos-Mitrakos <giorgos_mitrakos@yahoo.com>
2025-02-02 20:01:59 +0200

    fix brands

--- src/components/molecules/homepage/brandsBanner.tsx
+++ src/components/molecules/homepage/brandsBanner.tsx
@@ -27,35 +27,33 @@ const BrandsBanner = async ({ id, brands }: {
 }) => {
 
     return (
-        <section key={id} className='w-full'>
-            <h2 className="text-center text-siteColors-purple xs:text-2xl md:text-3xl font-bold rounded">Brands</h2>
-            <div className='w-screen px-16 overflow-x-auto'>
-                <div className='flex w-full items-center space-x-4'>
-                    {brands.data && brands.data.length > 0 &&
-                        brands.data.map(brand => (brand.attributes.logo.data &&
-                            <div key={brand.id} className='shrink-0'>
-                                <Link href={`/search?search=${brand.attributes.name}&κατεσκευαστής=${brand.attributes.name}`}>
-                                    {brand.attributes.logo.data ?
-                                        <p>
-                                            <Image
-                                                // className='object-contain'
-                                                // fill
-                                                height={48}
-                                                width={96}
-                                                src={getStrapiMedia(brand.attributes.logo.data.attributes.url)}
-                                                alt={brand.attributes.logo.data.attributes.alternativeText}
-                                                quality={75}
-                                                aria-label={brand.attributes.logo.data.attributes.alternativeText || ""}
-                                                blurDataURL={getStrapiMedia(brand.attributes.logo.data.attributes.url)}
-                                                placeholder="blur"
-                                            />
-                                        </p>
-                                        // <NextImage media={brand.attributes.logo.data.attributes} width={240} height={240} />
-                                        : <FaRegImage className="w-ayto h-20 self-center" />}
-                                </Link>
-                            </div>
-                        ))}
-                </div>
+        <section key={id} className='w-full '>
+            <h2 className="text-center text-siteColors-purple dark:text-slate-200 xs:text-2xl md:text-3xl font-bold rounded">Brands</h2>
+            <div className='flex items-center space-x-4 w-full overflow-x-auto dark:bg-slate-200 rounded-md mt-4 p-4'>
+                {brands.data && brands.data.length > 0 &&
+                    brands.data.map(brand => (brand.attributes.logo.data &&
+                        <div key={brand.id} className='shrink-0'>
+                            <Link href={`/search?search=${brand.attributes.name}&κατεσκευαστής=${brand.attributes.name}`}>
+                                {brand.attributes.logo.data ?
+                                    <p>
+                                        <Image
+                                            // className='object-contain'
+                                            // fill
+                                            height={36}
+                                            width={96}
+                                            src={getStrapiMedia(brand.attributes.logo.data.attributes.url)}
+                                            alt={brand.attributes.logo.data.attributes.alternativeText}
+                                            quality={75}
+                                            aria-label={brand.attributes.logo.data.attributes.alternativeText || ""}
+                                            blurDataURL={getStrapiMedia(brand.attributes.logo.data.attributes.url)}
+                                            placeholder="blur"
+                                        />
+                                    </p>
+                                    // <NextImage media={brand.attributes.logo.data.attributes} width={240} height={240} />
+                                    : <FaRegImage className="w-ayto h-20 self-center" />}
+                            </Link>
+                        </div>
+                    ))}
             </div>
         </section>
     )
