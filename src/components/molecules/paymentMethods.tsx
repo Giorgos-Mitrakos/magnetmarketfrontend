1b43c52d7b573bbcf248f82723fd953939e99d1b
Giorgos-Mitrakos <giorgos_mitrakos@yahoo.com>
2025-02-06 11:17:35 +0200

    fix payment range

--- src/components/molecules/paymentMethods.tsx
+++ src/components/molecules/paymentMethods.tsx
@@ -7,12 +7,8 @@ import { forwardRef, useContext, useEffect, useImperativeHandle } from "react"
 import { ShippingContext } from "@/context/shipping"
 import { useFormik } from "formik"
 import * as Yup from 'yup'
-import piraeusLogo from "../../../public/piraeus-logo-2024.png";
-import alphaBankLogo from "../../../public/alphaBank_logo.svg";
-import eurobankLogo from "../../../public/eurobank_logo.svg";
-import nbgLogo from "../../../public/nbg-logo.svg";
-import Image from "next/image"
 import Banks from "../atoms/banks"
+import { CartContext } from "@/context/cart"
 
 export type PaymentMethodsRef = {
     submitForm: () => void;
@@ -45,6 +41,7 @@ interface IPaymentMethods {
 
 const PaymentMethods = forwardRef<PaymentMethodsRef>((props, ref) => {
     const { paymentMethod, savePaymentMethod, shippingMethod } = useContext(ShippingContext)
+    const { cartTotal } = useContext(CartContext)
     const { data: paymentMethodsData, loading: loadingPaymentMethods, error: errorPaymentMethods } = useQuery({ query: GET_PAYMENT_METHODS, jwt: '' })
 
     const paymentMethods = paymentMethodsData as IPaymentMethods
@@ -103,6 +100,9 @@ const PaymentMethods = forwardRef<PaymentMethodsRef>((props, ref) => {
                     if (shippingMethod.pickup && method.attributes.name === "Αντικαταβολή") {
                         return false
                     }
+                    if (method.attributes.range && method.attributes.range.maximum < cartTotal)
+                        return false
+
                     return true
                 }).map(method => (
                     <li key={method.id} className="flex items-center text-sm t space-x-2">
