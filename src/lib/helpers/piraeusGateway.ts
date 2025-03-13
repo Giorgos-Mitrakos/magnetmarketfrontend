3deeb4c5781d0dbade2d1a424773623d78cc0f39
Giorgos-Mitrakos <giorgos_mitrakos@yahoo.com>
2025-03-13 21:39:14 +0200

    save ticket

--- src/lib/helpers/piraeusGateway.ts
+++ src/lib/helpers/piraeusGateway.ts
@@ -1,17 +1,55 @@
 'use server'
 
+import { data } from 'autoprefixer';
 import { HmacSHA512,MD5 } from 'crypto-js';
 import HmacSha256 from 'crypto-js/hmac-sha256'
-import {env} from 'process'
+import {env, title} from 'process'
 const soap = require('soap');
+
+async function sendEmail({title,data}:{title:string,data:string}){
+    try {
+        const myInitData = {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json', },
+            body: JSON.stringify({
+                to:'giorgos_mitrakos@yahoo.com',
+                subject:title,
+                text:data
+            })
+        };
+
+        await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/order/sendEmail`,
+            myInitData,
+        )
+    } catch (error) {
+        
+    }
+}
+
+async function saveTicket({orderId,TranTicket}:{orderId:number,TranTicket:string}){
+    try {
+        await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/order/saveTicket`,
+            {
+                    method: "POST",
+                    headers: { 'Content-Type': 'application/json', },
+                    body: JSON.stringify({
+                        orderId:orderId,
+                        TransTicket:TranTicket,
+                    })
+                }
+            )
+    } catch (error) {
+        
+    }
+}
 
 export async function getTransactionTicket({ orderId, amount, installments }: { orderId: number, amount: number, installments: number }) {
 
     const soapTicketData = {Request:{
-        Username: process.env.PEIRAIWS_USERNAME?process.env.PEIRAIWS_USERNAME:'MA312637',
-        Password: process.env.PEIRAIWS_PASSWORD?MD5(process.env.PEIRAIWS_PASSWORD).toString():MD5('TE212132').toString(),
-        MerchantId: process.env.MERCHANT_ID ? parseInt(process.env.MERCHANT_ID) : 2141425445,
-        PosId: process.env.POS_ID ? parseInt(process.env.POS_ID) : 2138072006,
+        Username: process.env.PEIRAIWS_USERNAME,
+        Password: MD5(`${process.env.PEIRAIWS_PASSWORD}`).toString(),
+        MerchantId: process.env.MERCHANT_ID,
+        PosId: parseInt(`${process.env.POS_ID}`),
         AcquirerId: process.env.ACQUIRER_ID,
         MerchantReference: orderId,        
         RequestType: '02',        
@@ -23,15 +61,14 @@ export async function getTransactionTicket({ orderId, amount, installments }: { 
         Parameters: ''
     }}
 
-        console.log(soapTicketData)
-
     try {
         const url = 'https://paycenter.piraeusbank.gr/services/tickets/issuer.asmx?WSDL';
 
-        soap.createClient(url, function(err:any, client:any) {
+        soap.createClient(url, async function(err:any, client:any) {
             
             if (err) {
                 console.error(err);
+                await sendEmail({title:"Αποτυχημένο request",data:err.toString()})
             } else {
                 // SOAP client object is created successfully
                 client.IssueNewTicket(soapTicketData,async function(err:any, result:any) {
@@ -39,42 +76,27 @@ export async function getTransactionTicket({ orderId, amount, installments }: { 
                     if (err) {
                         // console.error("ERRORRRR:",err, err.root?.Envelope?.Body);
                         const data=await err.toString()
-                        const myInitData = {
-                            method: "POST",
-                            headers: { 'Content-Type': 'application/json', },
-                            body: JSON.stringify({
-                                to:'giorgos_mitrakos@yahoo.com',
-                                    subject:"Αποτυχημένο request",
-                                    text:data
-                            })
-                        };
-
-                        await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/order/sendEmail`,
-                            myInitData,
-                        )
+                        
                     } else {
                         // SOAP request is successful, and r
                         // esult contains the response data
                         const data=JSON.stringify(result.IssueNewTicketResult)
-                        const myInitData = {
-                            method: "POST",
-                            headers: { 'Content-Type': 'application/json', },
-                            body: JSON.stringify({
-                                to:'giorgos_mitrakos@yahoo.com',
-                                    subject:"Επίτυχημένο request",
-                                    text:data
-                            })        
-                        };
 
-                        await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/order/sendEmail`,
-                            myInitData,
-                        )
+                        if(result.IssueNewTicketResult.ResultCode===0)
+                        {
+                            await saveTicket({orderId:orderId,TranTicket:result.IssueNewTicketResult.TranTicket})
+
+                            await sendEmail({title:"Επίτυχημένο request",data:data})
+                            
+                        }
+                        else{
+                            await sendEmail({title:"Αποτυχημένο request",data:data})
+                        }
                     }
                 });
             }
         });
 
-        // console.log("response:", response)
 
     
     } catch (error) {
