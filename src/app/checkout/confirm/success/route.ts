6128bb92f727377b8cc33488db70e9c399ace138
Giorgos-Mitrakos <giorgos_mitrakos@yahoo.com>
2025-03-17 10:11:53 +0200

    info sended

--- src/app/checkout/confirm/success/route.ts
+++ src/app/checkout/confirm/success/route.ts
@@ -4,15 +4,34 @@ import { sendEmail } from "@/lib/helpers/piraeusGateway";
 import { title } from "process";
 
 export async function POST(request: NextRequest) {
-    console.log(request.headers.get('content-type'))
+    
     const transactionData = await request.formData();
-    console.log('Transaction data received:', transactionData);
 
-    // Process the transaction (e.g., validate, save to database)
-    // if (transactionData.status === 'success') {
-    //     console.log('Transaction was successful!');
-    // }
-    sendEmail({ title: "Response from bank", data: request.headers.get('content-type') || "empty" })
+    const response = {
+        SupportReferenceID: transactionData.get('SupportReferenceID'),
+        ResultCode: transactionData.get('ResultCode'),
+        ResultDescription: transactionData.get('ResultDescription'),
+        StatusFlag: transactionData.get('StatusFlag'),
+        ResponseCode: transactionData.get('ResponseCode'),
+        ResponseDescription: transactionData.get('ResponseDescription'),
+        LanguageCode: transactionData.get('LanguageCode'),
+        MerchantReference: transactionData.get('MerchantReference'),
+        TransactionDateTime: transactionData.get('TransactionDateTime'),
+        TransactionId: transactionData.get('TransactionId'),
+        CardType: transactionData.get('CardType'),
+        PackageNo: transactionData.get('PackageNo'),
+        ApprovalCode: transactionData.get('ApprovalCode'),
+        RetrievalRef: transactionData.get('RetrievalRef'),
+        AuthStatus: transactionData.get('AuthStatus'),
+        Parameters: transactionData.get('Parameters'),
+        HashKey: transactionData.get('HashKey'),
+        PaymentMethod: transactionData.get('PaymentMethod'),
+        TraceID: transactionData.get('TraceID'),
+    }
+
+    console.log(response)
+    
+    sendEmail({ title: "Response from bank", data: JSON.stringify(response) })
     redirect('/checkout/thank-you')
     return new Response(JSON.stringify({ message: 'Payment processed successfully' }), {
         status: 200,
