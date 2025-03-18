668c2b7f01edefa96f60c4efbb422ed979b3aa6d
Giorgos-Mitrakos <giorgos_mitrakos@yahoo.com>
2025-03-18 12:53:42 +0200

    response from bank

--- src/app/checkout/confirm/success/route.ts
+++ src/app/checkout/confirm/success/route.ts
@@ -4,42 +4,49 @@ import { checkAuthResponse, getTicket, sendEmail } from "@/lib/helpers/piraeusGa
 
 export async function POST(request: NextRequest) {
 
-    const transactionData = await request.formData();
+    try {
 
-    const response = {
-        SupportReferenceID: transactionData.get('SupportReferenceID'),
-        ResultCode: transactionData.get('ResultCode'),
-        ResultDescription: transactionData.get('ResultDescription'),
-        StatusFlag: transactionData.get('StatusFlag'),
-        ResponseCode: transactionData.get('ResponseCode'),
-        ResponseDescription: transactionData.get('ResponseDescription'),
-        LanguageCode: transactionData.get('LanguageCode'),
-        MerchantReference: transactionData.get('MerchantReference'),
-        TransactionDateTime: transactionData.get('TransactionDateTime'),
-        TransactionId: transactionData.get('TransactionId'),
-        CardType: transactionData.get('CardType'),
-        PackageNo: transactionData.get('PackageNo'),
-        ApprovalCode: transactionData.get('ApprovalCode'),
-        RetrievalRef: transactionData.get('RetrievalRef'),
-        AuthStatus: transactionData.get('AuthStatus'),
-        Parameters: transactionData.get('Parameters'),
-        HashKey: transactionData.get('HashKey'),
-        PaymentMethod: transactionData.get('PaymentMethod'),
-        TraceID: transactionData.get('TraceID'),
+        const transactionData = await request.formData();
+
+        const response = {
+            SupportReferenceID: transactionData.get('SupportReferenceID'),
+            ResultCode: transactionData.get('ResultCode'),
+            ResultDescription: transactionData.get('ResultDescription'),
+            StatusFlag: transactionData.get('StatusFlag'),
+            ResponseCode: transactionData.get('ResponseCode'),
+            ResponseDescription: transactionData.get('ResponseDescription'),
+            LanguageCode: transactionData.get('LanguageCode'),
+            MerchantReference: transactionData.get('MerchantReference'),
+            TransactionDateTime: transactionData.get('TransactionDateTime'),
+            TransactionId: transactionData.get('TransactionId'),
+            CardType: transactionData.get('CardType'),
+            PackageNo: transactionData.get('PackageNo'),
+            ApprovalCode: transactionData.get('ApprovalCode'),
+            RetrievalRef: transactionData.get('RetrievalRef'),
+            AuthStatus: transactionData.get('AuthStatus'),
+            Parameters: transactionData.get('Parameters'),
+            HashKey: transactionData.get('HashKey'),
+            PaymentMethod: transactionData.get('PaymentMethod'),
+            TraceID: transactionData.get('TraceID'),
+        }
+
+        const res = JSON.stringify(response)
+
+        const ticket = await getTicket({ bankResponse: JSON.parse(res) })
+
+        const isResponseAuth = await checkAuthResponse({ bankResponse: JSON.parse(res), ticket: ticket.TranTicket.TranTicket })
+
+        // if (isResponseAuth) {
+        sendEmail({ title: "authenticated", data: `authenticated:${isResponseAuth}, ticket: ${ticket}, resposeFromBank: ${res}` })
+        // }
+        redirect('/checkout/thank-you')
+        return new Response(JSON.stringify({ message: 'Payment processed successfully' }), {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+
+    } catch (error) {
+        sendEmail({ title: "Error in Respone", data: `Error: ${error}` })
     }
-
-    const res = JSON.stringify(response)
-
-    const ticket = await getTicket({ bankResponse: JSON.parse(res) })
-
-    const isResponseAuth = await checkAuthResponse({ bankResponse: JSON.parse(res), ticket: ticket.TranTicket.TranTicket })
-
-    // if (isResponseAuth) {
-    sendEmail({ title: "authenticated", data: `authenticated:${isResponseAuth}, ticket: ${ticket}, resposeFromBank: ${res}` })
-    // }
-    redirect('/checkout/thank-you')
-    return new Response(JSON.stringify({ message: 'Payment processed successfully' }), {
-        status: 200,
-        headers: { 'Content-Type': 'application/json' },
-    });
 }
