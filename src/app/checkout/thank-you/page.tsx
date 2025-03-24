d6fc3ba279ac1f07b226d4b6e8acf62da0384708
Giorgos-Mitrakos <giorgos_mitrakos@yahoo.com>
2025-03-24 23:34:40 +0200

    thank-page again

--- src/app/checkout/thank-you/page.tsx
+++ src/app/checkout/thank-you/page.tsx
@@ -98,69 +98,64 @@ export default async function Success() {
     const lateDeviveryDate = addDays(deliveryDate, 6)
 
     return (
-        <div>
-            Success
-        </div>
+        <section className="rounded-lg p-4 bg-gradient-to-tr from-siteColors-lightblue via-siteColors-blue to-siteColors-pink mb-16">
+            <h1 className="text-2xl mb-4 font-semibold text-slate-200 text-center">Ευχαριστούμε για την παραγγελία!</h1>
+            <h2 className="text-xl mb-4 font-semibold text-slate-200 text-center">Αρ. {data.order.data.id}</h2>
+            <div className="flex justify-center flex-wrap gap-4">
+                {data && data.order.data.attributes.products.map(item => (
+                    <div key={item.id} className="grid grid-cols-5 h-60 w-96 shadow-md rounded-md bg-white">
+                        <div className="flex relative justify-center items-center p-4 col-span-2">
+                            {item.image ? <Image
+                                className="object-contain py-4 pl-4"
+                                key={item.id}
+                                src={`${process.env.NEXT_PUBLIC_API_URL}${item.image.data.attributes.formats.small ? item.image.data.attributes.formats.small.url : item.image.data.attributes.url}`}
+                                alt={item.name}
+                                // height={120}
+                                // width={120}
+                                fill
+                            /> :
+                                <FaRegImage className='h-40 w-40 text-siteColors-purple dark:text-slate-200' />}
+                        </div>
+                        <div className="flex flex-col justify-between col-span-3 p-4 space-y-2 text-gray-500">
+                            <h2 className="text-siteColors-purple font-semibold line-clamp-4 row-span-2">{item.name}</h2>
+                            <div className="flex flex-col">
+                                <div className="flex space-x-2">
+                                    <h3>Κωδικός:</h3>
+                                    <p className="text-left">{item.id}</p>
+                                </div>
+                                <div className="flex space-x-2">
+                                    <h3>Ποσότητα:</h3>
+                                    <p>{item.quantity}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className="rounded-lg shadow-md p-4 mt-4 text-gray-500 bg-white">
+                <div className="flex space-x-4">
+                    <h3>Τρόπος Πληρωμής:</h3>
+                    <p>{data.order.data.attributes.payment.name}</p>
+                </div>
+                <div className="flex space-x-4">
+                    <h3>Τρόπος Αποστολής:</h3>
+                    <p>{data.order.data.attributes.shipping.name}</p>
+                </div>
+                <div className="flex space-x-4">
+                    <h3>Πιθανή ημ/νια παράδοσης:</h3>
+                    <p>{earlyDeviveryDate.toLocaleDateString()} - {lateDeviveryDate.toLocaleDateString()}</p>
+                </div>
+                <div className="flex space-x-4">
+                    <h3>Σύνολο:</h3>
+                    <p>{data.order.data.attributes.total.toFixed(2)} €</p>
+                </div>
+            </div>
+            {data.order.data.attributes.payment.name === "Τραπεζική κατάθεση" && <div className="rounded-lg shadow-md p-4 mt-4 text-gray-500 bg-white">
+                <h2 className="text-xl mb-4 font-semibold text-siteColors-purple text-center">Τραπεζικοί Λογαριασμοί</h2>
+                <Banks />
+            </div>}
+        </section>
     )
-    // return (
-    //     <section className="rounded-lg p-4 bg-gradient-to-tr from-siteColors-lightblue via-siteColors-blue to-siteColors-pink mb-16">
-    //         <h1 className="text-2xl mb-4 font-semibold text-slate-200 text-center">Ευχαριστούμε για την παραγγελία!</h1>
-    //         <h2 className="text-xl mb-4 font-semibold text-slate-200 text-center">Αρ. {data.order.data.id}</h2>
-    //         <div className="flex justify-center flex-wrap gap-4">
-    //             {data && data.order.data.attributes.products.map(item => (
-    //                 <div key={item.id} className="grid grid-cols-5 h-60 w-96 shadow-md rounded-md bg-white">
-    //                     <div className="flex relative justify-center items-center p-4 col-span-2">
-    //                         {item.image ? <Image
-    //                             className="object-contain py-4 pl-4"
-    //                             key={item.id}
-    //                             src={`${process.env.NEXT_PUBLIC_API_URL}${item.image.data.attributes.formats.small ? item.image.data.attributes.formats.small.url : item.image.data.attributes.url}`}
-    //                             alt={item.name}
-    //                             // height={120}
-    //                             // width={120}
-    //                             fill
-    //                         /> :
-    //                             <FaRegImage className='h-40 w-40 text-siteColors-purple dark:text-slate-200' />}
-    //                     </div>
-    //                     <div className="flex flex-col justify-between col-span-3 p-4 space-y-2 text-gray-500">
-    //                         <h2 className="text-siteColors-purple font-semibold line-clamp-4 row-span-2">{item.name}</h2>
-    //                         <div className="flex flex-col">
-    //                             <div className="flex space-x-2">
-    //                                 <h3>Κωδικός:</h3>
-    //                                 <p className="text-left">{item.id}</p>
-    //                             </div>
-    //                             <div className="flex space-x-2">
-    //                                 <h3>Ποσότητα:</h3>
-    //                                 <p>{item.quantity}</p>
-    //                             </div>
-    //                         </div>
-    //                     </div>
-    //                 </div>
-    //             ))}
-    //         </div>
-    //         <div className="rounded-lg shadow-md p-4 mt-4 text-gray-500 bg-white">
-    //             <div className="flex space-x-4">
-    //                 <h3>Τρόπος Πληρωμής:</h3>
-    //                 <p>{data.order.data.attributes.payment.name}</p>
-    //             </div>
-    //             <div className="flex space-x-4">
-    //                 <h3>Τρόπος Αποστολής:</h3>
-    //                 <p>{data.order.data.attributes.shipping.name}</p>
-    //             </div>
-    //             <div className="flex space-x-4">
-    //                 <h3>Πιθανή ημ/νια παράδοσης:</h3>
-    //                 <p>{earlyDeviveryDate.toLocaleDateString()} - {lateDeviveryDate.toLocaleDateString()}</p>
-    //             </div>
-    //             <div className="flex space-x-4">
-    //                 <h3>Σύνολο:</h3>
-    //                 <p>{data.order.data.attributes.total.toFixed(2)} €</p>
-    //             </div>
-    //         </div>
-    //         {data.order.data.attributes.payment.name === "Τραπεζική κατάθεση" && <div className="rounded-lg shadow-md p-4 mt-4 text-gray-500 bg-white">
-    //             <h2 className="text-xl mb-4 font-semibold text-siteColors-purple text-center">Τραπεζικοί Λογαριασμοί</h2>
-    //             <Banks />
-    //         </div>}
-    //     </section>
-    // )
 
 }
 
@@ -174,7 +169,7 @@ export const metadata: Metadata = {
     openGraph: {
         url: 'www.magnetmarket.gr',
         type: 'website',
-        images: [`${process.env.NEXT_URL}/_next/static/media/MARKET MAGNET-LOGO.79db5357.svg`],
+        images: [`${process.env.NEXT_URL}/MARKET MAGNET-LOGO.svg`],
         siteName: "www.magnetmarket.gr",
         emails: ["info@magnetmarket.gr"],
         phoneNumbers: ['2221121657'],
