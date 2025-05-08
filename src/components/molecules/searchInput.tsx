4cd8bd15d7b3948d6e8ffa4959b1627197bda92c
Giorgos-Mitrakos <giorgos_mitrakos@yahoo.com>
2025-05-08 22:05:46 +0300

    fix search

--- src/components/molecules/searchInput.tsx
+++ src/components/molecules/searchInput.tsx
@@ -5,45 +5,48 @@ import { FaMicrophone, FaMicrophoneSlash } from "react-icons/fa";
 import useSpeechRecognition from "@/hooks/useSpeechRecognition";
 import { useRouter } from "next/navigation";
 import { sendGAEvent } from "@next/third-parties/google";
+import { ButtonHTMLAttributes } from "react";
 
 function SearchInput() {
     const router = useRouter();
 
     const { text, setText, isListening, startListening, stopListening, hasRecognitionSupport } = useSpeechRecognition()
-    
-    const handleClick = () => {
-        isListening ? stopListening : startListening
-      };
-    
-      const handleKeyDown = (event: React.KeyboardEvent<HTMLButtonElement>) => {
+
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+        isListening ? stopListening(e) : startListening(e)
+    };
+
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLButtonElement>) => {
         if (event.key === "Enter") {
-          event.preventDefault(); // Prevents the button from triggering on Enter
+            event.preventDefault(); // Prevents the button from triggering on Enter
         }
-      };
+    };
 
     return (
-        <div className="flex place-self-center w-full lg:w-1/2 dark:text-slate-800">
+        <div className="flex place-self-center border-b-2 border-siteColors-purple items-center w-full lg:w-1/2 dark:text-slate-800">
+            {hasRecognitionSupport &&
+                <button onClick={(e) => handleClick(e)} onKeyDown={handleKeyDown}
+                    className="flex text-siteColors-purple dark:text-slate-200 text-lg xs:text-xl md:text-2xl"
+                    aria-label="Ενεργοποίηση φωνητικής αναζήτησης"
+                >
+                    {isListening ?
+                        <FaMicrophone
+                            aria-label="Φωνητική αναζήτηση - Αναζητήστε ανά προϊόν, κατασκευαστή ή κατηγορία με φωνητικές εντολές!"
+                            className='text-red-600 text-2xl mb-1 ' /> :
+                        <FaMicrophoneSlash
+                            className='text-siteColors-purple dark:text-slate-800 text-2xl mb-1' />}
+                </button>}
             <form onSubmit={(e) => {
                 sendGAEvent('event', 'search', {
                     text
                 });
                 e.preventDefault()
                 router.push(`/search?search=${text}`)
+                setText("")
             }}
-                className="flex w-full justify-between border-b-2 p-2 rounded-sm bg-inherit dark:bg-slate-800 border-siteColors-purple"
+                className="flex w-full justify-between p-2 rounded-sm bg-inherit dark:bg-slate-800"
                 aria-label="Αναζήτηση">
-                {hasRecognitionSupport &&
-                    <button onClick={isListening ? stopListening : startListening} onKeyDown={handleKeyDown}
-                        className="flex text-siteColors-purple dark:text-slate-200 text-lg xs:text-xl md:text-2xl"
-                        aria-label="Ενεργοποίηση φωνητικής αναζήτησης"
-                    >
-                        {isListening ?
-                            <FaMicrophone
-                                aria-label="Φωνητική αναζήτηση - Αναζητήστε ανά προϊόν, κατασκευαστή ή κατηγορία με φωνητικές εντολές!"
-                                className='text-red-600 text-2xl mb-1 ' /> :
-                            <FaMicrophoneSlash
-                                className='text-siteColors-purple dark:text-slate-800 text-2xl mb-1' />}
-                    </button>}
+
                 <input type="search" tabIndex={0} placeholder="Αναζήτηση" value={text} onChange={(e) => setText(e.target.value)}
                     className="pl-2 bg-inherit  dark:text-slate-100 outline-none border-none flex-grow"
                     aria-label="Πεδίο αναζήτησης προϊόντων" />
